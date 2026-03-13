20de29f15ba4fedfe88fb6b6a022b72713859ac1
Ritika Nag <ritikanag@gmail.com>
2026-03-13 23:31:34 +0530

    multiple products

--- tests/addProduct.spec.ts
+++ tests/addProduct.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 import { ProductPage } from '../pages/addProduct';
 import { LoginPage } from '../pages/loginPage';
 import addProduct from '../test-data/addProduct.json'
+import users from '../test-data/users.json';
 
 let loginPage: LoginPage;
 let productPage: ProductPage; 
@@ -9,6 +10,9 @@ let productPage: ProductPage;
 test.beforeEach(async ({ page }) => {
   loginPage = new LoginPage(page);
   await loginPage.goto();
+  await page.fill('#user-name', users.validUser.username);
+  await page.fill('#password', users.validUser.password);
+  await page.click('#login-button');
 });
 
 test('add mmultiple product', async ({ page }) => {
